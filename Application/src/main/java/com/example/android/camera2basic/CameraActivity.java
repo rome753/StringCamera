4ab9b2592f2936bec4636a8174cfdfd457d01fae
chao <rome753@163.com>
2019-03-04 17:10:48 +0800

    添加ImageReader YUV解析方式

--- Application/src/main/java/com/example/android/camera2basic/CameraActivity.java
+++ Application/src/main/java/com/example/android/camera2basic/CameraActivity.java
@@ -27,7 +27,7 @@ public class CameraActivity extends AppCompatActivity {
         setContentView(R.layout.activity_camera);
         if (null == savedInstanceState) {
             getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, Camera2BasicFragment.newInstance())
+                    .replace(R.id.container, StringCameraFragment.newInstance())
                     .commit();
         }
     }
--- Application/src/main/java/com/example/android/camera2basic/Utils.java
+++ Application/src/main/java/com/example/android/camera2basic/Utils.java
@@ -7,6 +7,7 @@ import android.media.Image;
 import android.media.ImageReader;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.TextureView;
 import android.widget.TextView;
 
@@ -58,6 +59,43 @@ public class Utils {
         return null;
     }
 
+    public static String yuv2string(ImageReader imageReader) {
+        Image image = null;
+        ByteBuffer buffer = null;
+
+        try {
+            image = imageReader.acquireNextImage();
+            Image.Plane[] planes = image.getPlanes();
+            buffer = planes[0].getBuffer();
+            byte[] bytes = new byte[buffer.remaining()];
+            buffer.get(bytes);
+            int w = image.getWidth();
+            int h = image.getHeight();
+//            Log.e("chao", "planes " + planes.length + " " + w + "," + h);
+            StringBuilder sb = new StringBuilder();
+            for(int j = 0; j < h; j+=6) {
+                for (int i = 0; i < w; i+=6) {
+//                    int y = bytes[i * w + j] + 128;
+                    int y = bytes[j * w + i] + 128;
+                    char c = sChars[y * (sChars.length - 1) / 255];
+                    sb.append(c);
+                }
+                sb.append("\r\n");
+            }
+            return sb.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if(buffer != null) {
+                buffer.clear();
+            }
+            if(image != null) {
+                image.close();
+            }
+        }
+        return null;
+    }
+
     public static String bitmap2string(Bitmap bitmap) {
         StringBuilder sb = new StringBuilder();
         int w = bitmap.getWidth();
@@ -73,7 +111,8 @@ public class Utils {
     }
 
 //    private static char[] sChars = " .,:;i1tfLCG08@".toCharArray();
-    private static char[] sChars = "龘龍簇数菜米本木十大一　".toCharArray();
+//    private static char[] sChars = "龘龍簇数菜米本木十大一　".toCharArray();
+    private static char[] sChars = "　一大十木本米菜数簇龍龘".toCharArray();
 
     public static Character color2char(int color) {
         int red = Color.red(color);
